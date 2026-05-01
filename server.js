d5beeb7dcc293076da9d12707aac3fcb3a1c69c1
Breyon Rodrigues <breyonrodrigues2@gmail.com>
2026-05-02 01:42:14 +0530

    Fix Vercel views path

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
-// Set view engine to EJS
+const path = require('path');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Serve static files (CSS, Client-side JS, etc.)
